e02bdb419f6e92587af3e9cfa34bfee03f1af2d3
Roman De Meo <rdemeo@nero-networks.de>
2020-05-31 23:25:21 +0200

    v0.1.3

--- src/main/java/org/molkex/spring/minimalrest/socketchat/SocketChat.java
+++ src/main/java/org/molkex/spring/minimalrest/socketchat/SocketChat.java
@@ -2,14 +2,19 @@ package org.molkex.spring.minimalrest.socketchat;
 
 import org.molkex.spring.minimalrest.MinimalMessageBroker;
 import org.molkex.spring.minimalrest.MinimalRestApp;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.servlet.error.BasicErrorController;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
@@ -17,11 +22,11 @@ import java.util.stream.Collectors;
 
 @EnableWebSocket
 @RestController
-@RequestMapping("/")
+@RequestMapping("/chat/")
 public class SocketChat extends MinimalMessageBroker {
-    private static final String TOPIC = "/chat/**";
 
-    List<String> history = new ArrayList<>();
+    protected List<String> history = new ArrayList<>();
+    protected String topic = "/chat/";
 
     @GetMapping("/history")
     public List<String> getHistory() {
@@ -29,8 +34,9 @@ public class SocketChat extends MinimalMessageBroker {
     }
 
     @Override
-    protected void handle(Client client, Message message) {
-        if (TOPIC.equals(message.getTopic()) && !message.getPayload().contains("users")) {
+    protected void handle(MinimalMessageBroker.Client client, MinimalMessageBroker.Message message) {
+        if (message.getTopic().equals(topic+"**")
+                && !message.getPayload().contains("users\":[")) {
             history.add(message.getPayload());
             if (history.size() > 20)
                 history.remove(0);
@@ -39,29 +45,36 @@ public class SocketChat extends MinimalMessageBroker {
     }
 
     @Override
-    protected void afterSubscribe(Client client, Message message) {
+    protected void afterSubscribe(MinimalMessageBroker.Client client, MinimalMessageBroker.Message message) {
         broadcast();
     }
 
     @Override
-    protected void afterClose(Client client) {
+    protected void afterClose(MinimalMessageBroker.Client client) {
         broadcast();
     }
 
     private void broadcast() {
-        publish("/chat/**", new UsersBroadcast(
+        publish(topic +"**", new ChatMessage(
                 getTopics()
                         .stream()
-                        .filter(t -> t.startsWith("/chat/"))
-                        .map(t -> t.substring(6))
+                        .filter(t -> t.startsWith(topic))
+                        .map(t -> t.substring(topic.length()))
                         .collect(Collectors.toSet())));
     }
 
-    static class UsersBroadcast {
-        private Set<String> users;
+    public static class ChatMessage {
+        public ChatMessage() {}
 
-        UsersBroadcast(Set<String> users) {
+        ChatMessage(Set<String> users) {
             this.users = users;
+        }
+
+        public ChatMessage(String user, String text, boolean direct) {
+            this();
+            this.user = user;
+            this.text = text;
+            this.direct = direct;
         }
 
         public Set<String> getUsers() {
@@ -71,12 +84,74 @@ public class SocketChat extends MinimalMessageBroker {
         public void setUsers(Set<String> users) {
             this.users = users;
         }
+
+        public boolean direct;
+        public Set<String> users;
+        public String user;
+        public String text;
+    }
+
+    public static class ChatBot {
+
+        protected SocketChat chat;
+        protected List<String> users = new ArrayList<>();
+        protected String topic;
+        protected String user;
+
+        public ChatBot(String topic, String user, SocketChat chat) {
+            this.chat = chat;
+            this.topic = topic;
+            this.user = user;
+
+            chat.subscribe(topic+user, ChatMessage.class, m -> {
+                if (m.users != null) {
+                    onUsers(m);
+                } else if (m.direct) {
+                    onMessage(m);
+                }
+            });
+        }
+
+        protected void onMessage(ChatMessage chatMessage) {}
+
+        protected void onNewUser(String user) {}
+
+        protected void onUsers(ChatMessage m) {
+            Set<String> list = m.users.stream().filter(u -> !u.equals(user)).collect(Collectors.toSet());
+            list.stream().filter(u -> !users.contains(u)).forEach(this::onNewUser);
+
+            users.clear();
+            users.addAll(list);
+        }
+
+        protected void publish(String rcpt, String text) {
+            chat.publish(topic+rcpt, new ChatMessage(user, text, !rcpt.endsWith("*")));
+        }
     }
 
     @SpringBootApplication
-    public static class App extends MinimalRestApp {
+    public static class App extends MinimalRestApp implements HandlerInterceptor {
         public static void main(String[] args) {
             SpringApplication.run(App.class, args);
+        }
+
+        @Bean
+        ChatBot socketChatBot(@Autowired SocketChat socketChat) {
+            return new ChatBot("/chat/", "bot", socketChat) {
+                @Override
+                protected void onNewUser(String user) {
+                    publish(user,"Hello "+user+",\nwelcome to our chat\nonline are: "+users);
+                }
+
+                @Override
+                protected void onMessage(ChatMessage msg) {
+                    if (msg.text.contains("time"))
+                        publish(msg.user, "Hi "+msg.user+"! It is "+ LocalDateTime.now().format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM))+" now");
+
+                    else
+                        publish(msg.user,"Hi "+msg.user+"! Currently... I don't know how to do that, sorry.");
+                }
+            };
         }
     }
 }
--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,49 +1,57 @@
 window.onload = ()=> {
-    var user;
-
-    while (!location.search) {
-        if (user = prompt("Enter user name")) {
-            location.assign(location.href+'?'+user)
-            return
-        }
-    }
-
-    user = location.search.substr(1)
-
     var ele = (s, p)=> (p||document).querySelector(s),
-        ul = ele("ul"), inp = ele("input"), users = ele("#users"), status = ele("#status"),
-        client = MessageBrokerClient({
-            connected: ()=> client.send("SUBSCRIBE", "/chat/"+user),
+        topic = "/chat/",
+        user = minimalrest.queryPrompt("Enter user name"),
+        root = ele(".socket-chat"),
+        client = minimalrest.MessageBrokerClient({
+            connected: ()=> client.send("SUBSCRIBE", topic+user),
             status: (txt)=> status.textContent = txt,
             handle: (msg)=> msg.users ? renderUsers(msg) : renderMsg(msg)
         }),
-        renderUsers = (msg)=> {
-            users.textContent = msg.users.filter((u)=> u != user).join(", ")
-        },
+        channel = "",
+        ul = ele(".list", root),
+        inp = ele(".input", root),
+        users = ele(".users", root),
+        status = ele(".status", root),
+
         renderMsg = (msg)=> {
             var li = document.createElement("li")
             li.innerHTML = "<pre><strong></strong>: </pre>"
 
-            ele("pre strong", li).textContent = msg.user
+            ele("pre strong", li).textContent =
+                !msg.direct ? msg.user : msg.user == user ? user+channel : msg.user+"@"+user
+
             ele("pre", li).appendChild(document.createTextNode(msg.text))
 
             ul.appendChild(li)
             li.scrollIntoView()
+        },
+        renderUsers = (msg)=> {
+            users.textContent = msg.users.filter((u)=> u != user).join(", ")
+        },
+        send = (text)=> {
+            if (text = text || inp.value) {
+                var rcpt = "**", msg = text, m, data = {user: user}
+                channel = ""
+                if (m = text.match(/^@([^: ]*)?/)) {
+                    rcpt = m[1]
+                    channel = "@"+rcpt+" "
+                    msg = msg.substr(rcpt.length+1).trim()
+                    data.direct = !channel.endsWith("*")
+                }
+                data.text = msg
+                if (rcpt && msg) {
+                    if (data.direct) renderMsg(data)
+                    client.send("PUBLISH", topic+rcpt, data)
+                }
+                if (text == inp.value) inp.value = channel
+            }
         }
 
     inp.focus()
+    inp.onkeypress = (event)=> {event.which == 13 && send()}
 
-    fetch("/history")
+    fetch(topic+"history")
         .then((res)=> res.json())
         .then((list)=> list.forEach((msg)=> renderMsg(JSON.parse(msg))))
-
-    window.app = {
-        send: (text)=> {
-            if (text = text || inp.value) {
-                client.send("PUBLISH", "/chat/**", {text: text, user: user})
-                if (text == inp.value) inp.value = ""
-            }
-        }
-    }
-
 }
